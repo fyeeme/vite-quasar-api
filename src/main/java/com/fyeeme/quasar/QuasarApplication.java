d31677e8d26ca5c61a90d651fc140df7374979d4
LiuYang <fyeeme@gmail.com>
2021-12-18 14:13:36 +0800

    feat: init project

---
+++ src/main/java/com/fyeeme/quasar/QuasarApplication.java
@@ -0,0 +1,13 @@
+package com.fyeeme.quasar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuasarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuasarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fyeeme/quasar/QuasarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fyeeme.quasar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuasarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
